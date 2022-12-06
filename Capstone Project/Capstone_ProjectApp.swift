ce29a2d0e0ccb16d226dfd3ba36743a08f2cb785
septe habudin <septe.vhirgo@gmail.com>
2022-12-06 07:25:59 +0700

    initial project

---
+++ Capstone Project/Capstone_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Capstone_ProjectApp.swift
+//  Capstone Project
+//
+//  Created by septe habudin on 06/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Capstone_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
